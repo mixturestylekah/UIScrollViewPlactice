685ac9c2fe5f4a3d7cb45360f3ae34eb787dd63a
kentaro <mixture.style.k.a.h@gmail.com>
2016-05-23 17:52:40 +0900

    add close_button

--- UIScrollViewPractice/ViewController.swift
+++ UIScrollViewPractice/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var myScrollView: UIScrollView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myScrollView.contentSize = CGSizeMake(self.view.frame.size.height, self.view.frame.size.width)
+        let myImageView = UIImageView()
+        myScrollView.contentSize = CGSizeMake(1000, 1000)
+        
+        myImageView.image = UIImage(named: "pug.png")
+        myImageView.frame = CGRectMake(0, 0, 1000, 1000)
+        myScrollView.addSubview(myImageView)
+        
     }
 
     override func didReceiveMemoryWarning() {
